760bf8efccc93e6ce8fef41c40252336689d94c1
ashish tripathi <105139118+ashish270120003@users.noreply.github.com>
2025-09-21 09:47:40 +0530

    Add files via upload

---
+++ SQL Zomato Project - File/Easy Proctice Zomato Dataset.sql
@@ -0,0 +1,24 @@
+-- Easy Business Problems
+
+1. Find the total number of orders each restaurant has received.
+2. Calculate the average amount spent by each customer.
+3. Find the total sales amount for each restaurant.
+4. Identify the order with the highest number of items.
+5. Find the top 5 customers who have placed the most orders.
+6. Retrieve orders placed in the last 30 days.
+7. Calculate the average delivery time for each rider.
+8. List restaurants that are open 24 hours.
+9. Get the count of each order status.
+10. Find out how many deliveries each rider has completed.
+11. Identify the top 5 restaurants with the highest total sales.
+12. Get the number of orders per city.
+13. Find the most frequently ordered item.
+14. List orders where the delivery was completed on time.
+15. Calculate the average order amount for each day of the week.
+16. List customers who have placed orders in the last 90 days.
+17. Get delivery times and statuses for completed deliveries.
+18. Find restaurants that have not received any orders.
+19. Calculate the total number of orders for each month.
+20. Find the order with the longest time from order placement to delivery.
+
+*/
